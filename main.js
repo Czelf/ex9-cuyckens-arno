8497d81d6425e9238b0afbfc29132a31496213b4
arno cuyckens <r0372562@student.thomasmore.be>
2017-01-04 19:21:33 +0100

    voorbeeld books vanuit de les (books aangemaakt)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  */
 
 var express =  require('express'); // eenvoudige  webserver in node js
-var parser = require('body-parser'); // slaat input op als javascript object
+var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen (slaat input op als javascript object)
 
 var app = express();
 app.use (parser.json());
@@ -12,5 +12,14 @@ app.get('/', function (request, response) {
     response.send("hello world!");
 });
 
+var Book = function(id, name){
+    this.id = id;
+    this.name = name;
+};
+
+app.get('/books', function (request, response) {
+    var books = [new Book(1, 'harry potter'), new Book (2, 'Blinker')];
+    response.send(books);
+});
 
 app.listen(4321);
