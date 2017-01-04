21695ea56971f32197e4bbb86236d6c93c74e5f1
arno cuyckens <r0372562@student.thomasmore.be>
2017-01-04 17:02:31 +0100

    express,body parser en poort 4321 toegevoegd

--- main.js
+++ main.js
@@ -2,4 +2,12 @@
  * Created by arnoc on 4/01/2017.
  */
 
+var express =  require('express'); // eenvoudige  webserver in node js
+var parser = require('body-parser'); // slaat input op als javascript object
+
+var app = express();
+app.use (parser.json());
+
 console.log("hello world");
+
+app.listen(4321);
